457797b52c69ee3c0010220636221f9f46d430f0
Asya2307 <asya2307@mail.ru>
2022-04-12 19:02:50 +0500

    fix App.js

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,8 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header'
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
